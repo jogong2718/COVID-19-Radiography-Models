579101451a1edf6450c69b842d5c7c5b4e42ecb0
jogong2718 <82681643+jogong2718@users.noreply.github.com>
2022-10-25 20:16:29 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="Covid-Radiography-Models",
+  author="Jonathan Gong",
+  author_email="jonathangong2005@gmail.com",
+  license="MIT license",
+  description="This package provides AI-driven solutions for Classification and Image Segmentation of Covid-19 X-rays",
+  packages=["Processes"]
+)
